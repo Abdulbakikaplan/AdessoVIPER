04ab5d78a55223ec43612105f482e1a21626ab92
Abdülbaki Kaplan <abdulbakikaplan@hotmail.com>
2020-01-10 19:18:59 +0300

    initial commit

---
+++ AdessoVIPER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdessoVIPER
+//
+//  Created by Abdülbaki Kaplan on 1/10/20.
+//  Copyright © 2020 Abdülbaki Kaplan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
