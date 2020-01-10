6ce16b6fded5ed7a2e31f9d88a99cbca869067a2
Abdülbaki Kaplan <abdulbakikaplan@hotmail.com>
2020-01-10 19:37:54 +0300

    List VC Created

---
+++ AdessoVIPER/Scenes/ListVC/ListVC.swift
@@ -0,0 +1,30 @@
+//
+//  ListVC.swift
+//  AdessoVIPER
+//
+//  Created by Abdülbaki Kaplan on 1/10/20.
+//  Copyright © 2020 Abdülbaki Kaplan. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
