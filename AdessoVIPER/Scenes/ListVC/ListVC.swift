1b493813054b6073116084d86e26d71c179f27ab
Abdülbaki Kaplan <abdulbakikaplan@hotmail.com>
2020-01-10 19:43:03 +0300

    ListVC UI complete

--- AdessoVIPER/Scenes/ListVC/ListVC.swift
+++ AdessoVIPER/Scenes/ListVC/ListVC.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ListVC: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +18,5 @@ class ListVC: UIViewController {
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
